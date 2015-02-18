03c5d25d02c895e07f4c0fc61278f933c0b7624e
David Deneroff <dgdeneroff@gmail.com>
2015-02-18 12:11:25 -0500

    Add CRUD operations for comment resource

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,3 +3,9 @@ delete '/comments/:id/delete' do
   @comment.destroy
   redirect '/excuses'
 end
+
+post "/comment/:id/create" do
+  p params
+  Comment.create(content: params[:comment][:content], user_id: current_user.id, excuse_id: params[:id])
+  redirect '/excuses'
+end
