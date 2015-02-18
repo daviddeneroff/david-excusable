4a69f172c9e9da656486e85ad7963d3d930a2789
David Deneroff <dgdeneroff@gmail.com>
2015-02-18 09:52:04 -0500

    Add login/signup authentication with erb view

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -5,3 +5,17 @@ end
 get '/login' do
   erb :login
 end
+
+post '/login' do
+  if @user = User.find_by(username: params[:user][:username]).try(:authenticate, params[:user][:password])
+    session[:user_id] = @user.id
+    redirect "/excuses"
+  else
+    redirect '/login'
+  end
+end
+
+post '/signup' do
+  User.create(params[:user])
+  redirect '/login'
+end
