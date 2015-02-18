f3f4cd1f5108096e2bb67a53668deb2f0bde2247
David Deneroff <dgdeneroff@gmail.com>
2015-02-18 12:37:45 -0500

    Add ajax set up. Does not yet go to a route

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('.click-vote').click(function(event){
+    event.preventDefault();
+    console.log("wuuhhuu");
+    $.ajax({
+      type: "post",
+      url: "FIND IT",
+      success: function(success) {
+        console.log(success)
+      }
+    });
+  });
 });
