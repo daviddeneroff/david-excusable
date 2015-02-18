552c7f77bebed63d2eb46e8195bba0b12f2291f0
David Deneroff <dgdeneroff@gmail.com>
2015-02-18 10:53:09 -0500

    Add ability to make an excuse

--- app/controllers/excuses.rb
+++ app/controllers/excuses.rb
@@ -3,6 +3,15 @@ get '/excuses' do
   erb :'excuses/all'
 end
 
+post '/excuses/create' do
+  Excuse.create(title: params[:excuse][:title], content: params[:excuse][:content], user_id: session[:user_id])
+  redirect '/excuses'
+end
+
+get '/excuses/new' do
+  erb :'excuses/new'
+end
+
 get '/excuses/:id' do
   @excuse = Excuse.find(params[:id])
   erb :'excuses/single'
