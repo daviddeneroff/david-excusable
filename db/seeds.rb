babd3328898603a13c459c05f0120cca5c3faa9b
David Deneroff <dgdeneroff@gmail.com>
2015-02-18 19:06:47 -0500

    made more fitting seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ User.create(username: "david", password:"123", password_confirmation: "123")
 User.create(username: "mike", password: "123", password_confirmation: "123")
 User.create(username: "john", password: "123", password_confirmation: "123")
 
-Excuse.create(title: "deli", content:"riva deli", user_id: 1)
-Excuse.create(title: "hello", content: "how are you", user_id: 2)
-Excuse.create(title: "hey man", content: "sup bru", user_id: 3)
+Excuse.create(title: "train delays", content:"ahhh the train is stuck. will be 10 mins late", user_id: 1)
+Excuse.create(title: "snow", content: "snow is making traffic crazy...ill do the best i can", user_id: 2)
+Excuse.create(title: "thirsty", content: "had to grab a smoothie. will be there when i get there", user_id: 3)
 
 Comment.create(content: "nice one man", user_id: 1, excuse_id: 2)
 Comment.create(content: "no way", user_id: 2, excuse_id: 3)
