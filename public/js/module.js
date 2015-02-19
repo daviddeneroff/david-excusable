9c972b79102e9c75818fef08d742ae79df420da4
David Deneroff <dgdeneroff@gmail.com>
2015-02-18 20:00:00 -0500

    Clean up js module file

--- public/js/module.js
+++ public/js/module.js
@@ -1,23 +1,3 @@
-// var curseChecker = (function() {
-//   var curseEval = function(input) {
-//   var arr = ["booger", "icky"];
-//     for (var i = 0; i < arr.length; i++) {
-//       arr[i]
-//     }
-//   };
-//   return {
-//     look: curseEval;
-//   }
-// })
-
-// var str = "texttexttext";
-// var arr = ["asd", "ghj", "xtte"];
-// for (var i = 0, len = arr.length; i < len; ++i) {
-//     if (str.indexOf(arr[i]) != -1) {
-//         // str contains arr[i]
-//     }
-// }
-
 var curseChecker = (function() {
   var curseEval = function(input) {
   var arr = ["booger", "ugly", "stupid"]
